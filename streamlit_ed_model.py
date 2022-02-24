6e33f86cb7845ce2cc46369fb466e73167324b4e
Rchramos <98285467+Rchramos@users.noreply.github.com>
2022-02-23 19:32:12 -0500

    Add files via upload

--- streamlit_ed_model.py
+++ streamlit_ed_model.py
@@ -8,34 +8,38 @@ from bokeh.palettes import Set1_5
 from bokeh.models import ColumnDataSource
 
 from st_aggrid import AgGrid
-import EDSIM_BackEnd.ED_Model2 as Model
+import EDSIM_BackEnd.ED_Model3 as Model
 import EDSIM_BackEnd.Statistics as s
 
-#Page configurations
-#favicon = st.image
+# Page configurations
+# favicon = st.image
 st.set_page_config(
-     page_title="Emergency Department Simulation",
-     #page_icon=favicon,
-     layout="wide",
-     #initial_sidebar_state="expanded",
-     menu_items={
-         'About': "Ryerson Engineering Capstone Project created by: Gurvir, Mike, Renato, Sarmad"
-     }
- )
+    page_title="Emergency Department Simulation",
+    # page_icon=favicon,
+    layout="wide",
+    # initial_sidebar_state="expanded",
+    menu_items={
+        'About': "Ryerson Engineering Capstone Project created by: Gurvir, Mike, Renato, Sarmad"
+    }
+)
 
-#Title at the top 
+# Title at the top
 st.title('Emergency Department Simulation')
 
 # File Upload/Processing
 file = st.file_uploader('Upload .csv file with data')
+
+
 def process_file(file):
     st.write(file)
     df = pd.read_csv(file)
     st.write(df)
+
+
 if st.button('Process file'):
     process_file(file)
 
-#Inputting Fields/ Sliders for each category 
+# Inputting Fields/ Sliders for each category
 st.header('Process Service Times (mins)')
 col01, col02, col03, col04 = st.columns(4)
 with col01:
@@ -83,127 +87,127 @@ simPar_warmUp = st.number_input('Warm Up Period', 1, 30, 10)
 
 simParameters = {
     'resCapacity': {
-        'doctor':docs, 
-        'nurse':nurse,
-        'beds':beds,
-        'rBeds':resbeds, 
+        'doctor': docs,
+        'nurse': nurse,
+        'beds': beds,
+        'rBeds': resbeds,
 
-    }, 
-    'pInterArrival':{
-        'ambulance':walkInP, 
+    },
+    'pInterArrival': {
+        'ambulance': walkInP,
         'walkIn': AmbulanceP
 
-    }, 
-    'serTimes':{
-        'priorAssessment': Priorityass, 
-        'ctasAssessment':CTASass, 
-        'registration':Registration, 
-        'bedAssignment':Bedass,
-        'initialAssessment':Initialass,
-        'treatment':Treatment, 
-        'discharge':Dischargeass,
-        'resuscitation':Resus 
-    }, 
-    'ctasDist':{
+    },
+    'serTimes': {
+        'priorAssessment': Priorityass,
+        'ctasAssessment': CTASass,
+        'registration': Registration,
+        'bedAssignment': Bedass,
+        'initialAssessment': Initialass,
+        'treatment': Treatment,
+        'discharge': Dischargeass,
+        'resuscitation': Resus
+    },
+    'ctasDist': {
         'ambulance': {
-             1:0.5, 
-             2:0.2, 
-             3:0.3, 
-             4:0.1, 
-             5:0
-            
-        }, 
-        'walkIn':{
-             1:0.3, 
-             2:0.2, 
-             3:0.1, 
-             4:0.1, 
-             5:0.1
+            1: 0.5,
+            2: 0.2,
+            3: 0.3,
+            4: 0.1,
+            5: 0
+
+        },
+        'walkIn': {
+            1: 0.3,
+            2: 0.2,
+            3: 0.1,
+            4: 0.1,
+            5: 0.1
         }
 
-    }, 
-    'iter':simPar_iterations,
-    'warmUp':simPar_warmUp, 
-    'length':simPar_duration
+    },
+    'iter': simPar_iterations,
+    'warmUp': simPar_warmUp,
+    'length': simPar_duration
 }
 
+
 # Having issues with bokeh and GroupBy pandas data frames.
-def plots(df,losRange=(1000,1500)):
-        #Group dataframe by Run ID and CTAS Level
-        means = s.meanByGroup(df)
+def plots(df, losRange=(1000, 1500)):
+    # Group dataframe by Run ID and CTAS Level
+    means = s.meanByGroup(df)
 
-        #Mean LOS of grouped dataframe
-        meanLOS = s.meanParByCTASperRun(means,'los')
-        
-        CTAS1Data = s.getDataByCTASLevel(meanLOS,1)
-        CTAS2Data = s.getDataByCTASLevel(meanLOS,2)
-        CTAS3Data = s.getDataByCTASLevel(meanLOS,3)
-        CTAS4Data = s.getDataByCTASLevel(meanLOS,4)
-        CTAS5Data = s.getDataByCTASLevel(meanLOS,5)
+    # Mean LOS of grouped dataframe
+    meanLOS = s.meanParByCTASperRun(means, 'los')
 
-        mid_box = BoxAnnotation(bottom=losRange[0], top=losRange[1], fill_alpha=0.2, fill_color='#0072B2')
-        
-        losCTASData = [CTAS1Data,CTAS2Data,CTAS3Data,CTAS4Data,CTAS5Data]
-        graphNames = ['CTAS 1', 'CTAS 2', 'CTAS 3', 'CTAS 4', 'CTAS 5']
+    CTAS1Data = s.getDataByCTASLevel(meanLOS, 1)
+    CTAS2Data = s.getDataByCTASLevel(meanLOS, 2)
+    CTAS3Data = s.getDataByCTASLevel(meanLOS, 3)
+    CTAS4Data = s.getDataByCTASLevel(meanLOS, 4)
+    CTAS5Data = s.getDataByCTASLevel(meanLOS, 5)
 
-        p = figure(x_axis_label = 'RUN ID', y_axis_label = 'MINUTES')
-        p.title.text = 'Click on legend entries to hide the corresponding lines'
+    mid_box = BoxAnnotation(bottom=losRange[0], top=losRange[1], fill_alpha=0.2, fill_color='#0072B2')
 
-        for data,name,color in zip(losCTASData,graphNames,Set1_5):
-            p.line(data['los'].keys(),data['los'], line_width=2, color=color, alpha=0.8, legend_label=name)
+    losCTASData = [CTAS1Data, CTAS2Data, CTAS3Data, CTAS4Data, CTAS5Data]
+    graphNames = ['CTAS 1', 'CTAS 2', 'CTAS 3', 'CTAS 4', 'CTAS 5']
 
-            p.legend.location = "top_left"
-            p.legend.click_policy="hide"
+    p = figure(x_axis_label='RUN ID', y_axis_label='MINUTES')
+    p.title.text = 'Click on legend entries to hide the corresponding lines'
 
-        #Creates ColumnDataSource for Bokeh input
-        #source_grouped = ColumnDataSource(meanLOS)
-        #source_df = ColumnDataSource(df)
+    for data, name, color in zip(losCTASData, graphNames, Set1_5):
+        p.line(data['los'].keys(), data['los'], line_width=2, color=color, alpha=0.8, legend_label=name)
 
-        #p = figure()
-        #p.title.text = 'Click on legend entries to hide the corresponding lines'
-
-        #p.line(x='Run ID_CTAS', y='los', legend_label='Mean LoS', source=source_grouped)
-        #p.line(x='Run ID', y='los', legend_label='LoS', source=source_df)
         p.legend.location = "top_left"
-        p.legend.click_policy="hide"
+        p.legend.click_policy = "hide"
 
-        p.add_layout(mid_box)
+    # Creates ColumnDataSource for Bokeh input
+    # source_grouped = ColumnDataSource(meanLOS)
+    # source_df = ColumnDataSource(df)
 
-        return p
+    # p = figure()
+    # p.title.text = 'Click on legend entries to hide the corresponding lines'
 
-        #fig, axs = plt.subplots(5,figsize=(10,17))
-        
-        # Subplot for each CTAS level
-        # axs[0].plot(meanLOSforCTAS1, 'C0')
-        # axs[0].set_xlabel('Run ID')
-        # axs[0].set_ylabel('Mean length of stay (min)')
-        # axs[0].set_title('Mean Patient Length of Stay per Run ID (CTAS 1-5)')
+    # p.line(x='Run ID_CTAS', y='los', legend_label='Mean LoS', source=source_grouped)
+    # p.line(x='Run ID', y='los', legend_label='LoS', source=source_df)
+    p.legend.location = "top_left"
+    p.legend.click_policy = "hide"
 
-        # axs[1].plot(meanLOSforCTAS2, 'C1')
-        # axs[1].set_xlabel('Run ID')
-        # axs[1].set_ylabel('Mean length of stay (min)')
+    p.add_layout(mid_box)
 
-        # axs[2].plot(meanLOSforCTAS3, 'C2')
-        # axs[2].set_xlabel('Run ID')
-        # axs[2].set_ylabel('Mean length of stay (min)')
+    return p
 
-        # axs[3].plot(meanLOSforCTAS4, 'C3')
-        # axs[3].set_xlabel('Run ID')
-        # axs[3].set_ylabel('Mean length of stay (min)')
+    # fig, axs = plt.subplots(5,figsize=(10,17))
 
-        # axs[4].plot(meanLOSforCTAS5, 'C4')
-        # axs[4].set_xlabel('Run ID')
-        # axs[4].set_ylabel('Mean length of stay (min)')
+    # Subplot for each CTAS level
+    # axs[0].plot(meanLOSforCTAS1, 'C0')
+    # axs[0].set_xlabel('Run ID')
+    # axs[0].set_ylabel('Mean length of stay (min)')
+    # axs[0].set_title('Mean Patient Length of Stay per Run ID (CTAS 1-5)')
+
+    # axs[1].plot(meanLOSforCTAS2, 'C1')
+    # axs[1].set_xlabel('Run ID')
+    # axs[1].set_ylabel('Mean length of stay (min)')
+
+    # axs[2].plot(meanLOSforCTAS3, 'C2')
+    # axs[2].set_xlabel('Run ID')
+    # axs[2].set_ylabel('Mean length of stay (min)')
+
+    # axs[3].plot(meanLOSforCTAS4, 'C3')
+    # axs[3].set_xlabel('Run ID')
+    # axs[3].set_ylabel('Mean length of stay (min)')
+
+    # axs[4].plot(meanLOSforCTAS5, 'C4')
+    # axs[4].set_xlabel('Run ID')
+    # axs[4].set_ylabel('Mean length of stay (min)')
 
 
-#The graphs being displayed/modeled
+# The graphs being displayed/modeled
 
 if st.button('Run the Simulation'):
     # Gets results
     results_df = Model.runSim(simParameters)
     # Gets plots
 
-    
     # Shows the graphs
     st.title('Interactive Plots')
     plot = plots(results_df)
@@ -217,14 +221,10 @@ if st.button('Run the Simulation'):
     # Raw data frame
     st.title('Raw Simulation Resulting Data')
     AgGrid(results_df)
-    
+
     # Download button for results csv file
     st.download_button(
-        label="Download the Results as .CSV file", 
-        data=results_df.to_csv().encode('utf-8'), 
-        file_name='Simulation_Results.csv', 
+        label="Download the Results as .CSV file",
+        data=results_df.to_csv().encode('utf-8'),
+        file_name='Simulation_Results.csv',
         mime='text/csv')
-
-
-
-
