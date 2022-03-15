ef691fd74e62b8761f51d10835d74a245ec21c58
Rchramos <98285467+Rchramos@users.noreply.github.com>
2022-03-14 21:04:42 -0400

    Add files via upload

--- streamlit_ed_model.py
+++ streamlit_ed_model.py
@@ -46,20 +46,20 @@ if st.button('Process file'):
 st.header('Process Service Times (mins)')
 col01, colnull, col02, colnull, col03, colnull, col04 = st.columns([2,0.5,2,0.5,2,0.5,2])
 with col01:
-    CTASass = st.number_input('CTAS Assessment (STD)', 1, 50, 42)
-    Priorityass = st.number_input('Priority Assessment (STD)', 1, 50, 23)
-    Initialass = st.number_input('Initial Assessment (STD)', 1, 50, 42)
-    Dischargeass = st.number_input('Discharge (STD)', 1, 50, 23)
+    CTASass_std_dev = st.number_input('CTAS Assessment (STD)', 1, 50, 42)
+    Priorityass_std_dev = st.number_input('Priority Assessment (STD)', 1, 50, 23)
+    Initialass_std_dev = st.number_input('Initial Assessment (STD)', 1, 50, 42)
+    Dischargeass_std_dev = st.number_input('Discharge (STD)', 1, 50, 23)
 with col02:
     CTASass = st.number_input('CTAS Assessment (Mean)', 1, 50, 42)
     Priorityass = st.number_input('Priority Assessment (Mean)', 1, 50, 23)
     Initialass = st.number_input('Initial Assessment (Mean)', 1, 50, 42)
     Dischargeass = st.number_input('Discharge (Mean)', 1, 50, 23)
 with col03:
-    Treatment = st.number_input('Treatments (STD)', 1, 50, 20)
-    Bedass = st.number_input('Bed Assignment (STD)', 1, 50, 32)
-    Resus = st.number_input('Resuscitations (STD)', 1, 50, 19)
-    Registration = st.number_input('Registrations (STD)', 1, 50, 49)
+    Treatment_std_dev = st.number_input('Treatments (STD)', 1, 50, 20)
+    Bedass_std_dev = st.number_input('Bed Assignment (STD)', 1, 50, 32)
+    Resus_std_dev = st.number_input('Resuscitations (STD)', 1, 50, 19)
+    Registration_std_dev = st.number_input('Registrations (STD)', 1, 50, 49)
 with col04:
     Treatment = st.number_input('Treatments (Mean)', 1, 50, 20)
     Bedass = st.number_input('Bed Assignment (Mean)', 1, 50, 32)
@@ -116,6 +116,16 @@ simParameters = {
         'discharge': Dischargeass,
         'resuscitation': Resus
     },
+    'stdDeviations': {
+        'priorAssessment_Deviation': Priorityass_std_dev,
+        'ctasAssessment_Deviation': CTASass_std_dev,
+        'registration_Deviation': Registration_std_dev,
+        'bedAssignment_Deviation': Bedass_std_dev,
+        'initialAssessment_Deviation': Initialass_std_dev,
+        'treatment_Deviation': Treatment_std_dev,
+        'discharge_Deviation': Dischargeass_std_dev,
+        'resuscitation_Deviation': Resus_std_dev
+        },
     'ctasDist': {
         'ambulance': {
             1: 0.5,
