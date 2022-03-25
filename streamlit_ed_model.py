56dfd8bcc5315527be002cb19b7b15b533b6fa55
gurvirparmar <44628715+gurvirparmar@users.noreply.github.com>
2022-03-25 08:56:14 -0400

    Revert "bunch of changes front end"
    This reverts commit 507067f828e0673a234849418859451a7f83d6d2.

--- streamlit_ed_model.py
+++ streamlit_ed_model.py
@@ -9,11 +9,10 @@ from bokeh.models import ColumnDataSource
 from st_aggrid import AgGrid
 import EDSIM_BackEnd.ED_Model3 as Model
 import EDSIM_BackEnd.Statistics as stats
-from EDSIM_FrontEnd import GTResults, HelpPage
 
 
 from multiapp import MultiApp
-from EDSIM_BackEnd import Home, ExtraVariables
+from EDSIM_BackEnd import Home, ExtraVariables, GTResults, HelpPage
 
 #Page configurations
 # st.set_page_config(
