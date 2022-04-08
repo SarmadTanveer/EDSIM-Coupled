4c0728b93472a3b48b1bf43ab49894f0601e328e
kobty <michael.kobty@gmail.com>
2022-04-08 03:10:36 -0400

    Summary, Help, Welcome formatting
    Fixed summary, help, welcome formatting. Renamed files (HelpPage.py was the welcome page)

--- index.py
+++ index.py
@@ -3,13 +3,13 @@ import streamlit as st
 
 import EDSIM_BackEnd.ED_Model3 as Model
 from multiapp import MultiApp
-from EDSIM_FrontEnd import CreateScenario, Home, GTResults, HelpPage, FAQ
+from EDSIM_FrontEnd import CreateScenario, Help, Home, GTResults, Welcome
 
 st.set_page_config(layout="wide")
 
 app = MultiApp()
-app.add_page("Welcome", HelpPage.app)
-app.add_page("Help Page", FAQ.app)
+app.add_page("Welcome", Welcome.app)
+app.add_page("Help Page", Help.app)
 app.add_page("Create Scenario", CreateScenario.app)
 app.add_page("Run Created Scenario", Home.app)
 app.add_page("Graph and Table Results", GTResults.app)
