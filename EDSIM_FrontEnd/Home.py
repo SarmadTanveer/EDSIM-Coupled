e5cdf77bac80b6171247e968ed534f1ae98e5a7f
Rchramos <98285467+Rchramos@users.noreply.github.com>
2022-04-28 15:07:17 -0400

    Add files via upload

--- EDSIM_FrontEnd/Home.py
+++ EDSIM_FrontEnd/Home.py
@@ -28,22 +28,22 @@ def app():
         st.header('Resource allocation parameters')
         col1, col2, col3,col4 = st.columns(4)
         with col1:
-            docs = st.number_input('# of Doctors', 1, 20, 2, help="Min=381, Max=5000")
+            docs = st.number_input('# of Doctors', 1, 20, 2, help="Min=1, Max=20")
         with col2:
-            nurse = st.number_input('# of Nurses', 1, 20, 5, help="Min=381, Max=5000")
+            nurse = st.number_input('# of Nurses', 1, 20, 5, help="Min=1, Max=20")
         with col3:
-            beds = st.number_input('# of Beds', 1, 40, 10, help="Min=381, Max=5000")
+            beds = st.number_input('# of Beds', 1, 40, 10, help="Min=1, Max=40")
         with col4:     
-            resbeds = st.number_input('# of Resuscitation Bed', 1, 20, 5, help="Min=381, Max=5000")
+            resbeds = st.number_input('# of Resuscitation Bed', 1, 20, 5, help="Min=1, Max=20")
     
         st.header('Simulation parameters')
         col5, colnull, col6, colnull, col7 = st.columns([2,1,2,1,2])
         with col5: 
             simPar_duration = st.number_input('Duration (mins)', 1440,525600, 1440, help="Min=1440, Max=525600")
         with col6: 
-            simPar_iterations = st.number_input('Iterations', 1, 40, 18, help="Min=381, Max=5000")
+            simPar_iterations = st.number_input('Iterations', 1, 40, 18, help="Min=1, Max=40")
         with col7: 
-            simPar_warmUp = st.number_input('Warm Up Period', 1, 120, 10, help="Min=381, Max=5000")
+            simPar_warmUp = st.number_input('Warm Up Period', 1, 120, 10, help="Min=1, Max=120")
         
         submit_button = st.form_submit_button('Run Simulation')
     
